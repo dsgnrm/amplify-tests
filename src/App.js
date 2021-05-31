467daf03789255e939db89ae4826c83b0a00d499
mattt <matthewosborne@live.com>
2021-05-31 13:03:58 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { Box } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import MainApp from "./screens/MainApp";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
-  return <MainApp />;
+  return (
+    <>
+      <MainApp />
+      <AmplifySignOut />
+    </>
+  );
 }
 
-export default App;
+export default withAuthenticator(App);
